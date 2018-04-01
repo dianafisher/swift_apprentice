39a050fa8aa06531371c72cb392da43634a7c3ce
Diana Fisher <diana.fisher@gmail.com>
2018-04-01 14:55:55 -0700

    Add markup to Chapter 3 playground.

--- Chapter3.playground/Contents.swift
+++ Chapter3.playground/Contents.swift
@@ -16,7 +16,9 @@ let dogEqualsCat = guess == "cat"
 
 let order = "cat" < "dog"
 
-// MARK: Mini-exercises
+/*:
+ ## Mini Exercises
+ */
 
 // 1.
 let myAge = 14
@@ -51,7 +53,9 @@ let statement = (myAge >= 13 && myAge <= 19) ? "Teenager" : "Not a teenager"
 // 2.
 print (statement)
 
-// MARK: Notes
+/*:
+ ## Notes
+ */
 
 var sum = 1
 while sum < 1000 {
@@ -87,9 +91,22 @@ repeat {
     print("After \(counter) rolls, roll is \(roll)")
 } while roll != 0
 
-// MARK: Challenges
-
-// 1.
+/*:
+ ## Challenges
+ ### Challenge 1
+ What’s wrong with the following code?
+ 
+ ```
+ let firstName = "Matt"
+ 
+ if firstName == "Matt" {
+ let lastName = "Galloway"
+ } else if firstName == "Ray" {
+ let lastName = "Wenderlich"
+ }
+ let fullName = firstName + " " + lastName
+ ```
+ */
 let firstName = "Matt"
 var lastName = ""
 if firstName == "Matt" {
@@ -103,8 +120,8 @@ print(fullName)
 // 2.
 let answer = ((10/2) > 3) && ((10 % 2) == 0)
 
-// 3.
-/*
+/*:
+ ### Challenge 3
  Squares on a chessboard numbered left to right, top to bottom.
  0 is top-left square.  63 is bottom-right square.
  Rows numbered 0 to 7
@@ -118,12 +135,12 @@ let nextCol = (position.col + 1) % 8
 let next = (row: nextRow, col: nextCol)
 print("\(next)")
 
-// 4.
-/*
- Calculate the solution to the quadratic equation: ax^2 + bx + c = 0
- given the coefficients a, b, and c.
+/*:
+ ### Challenge 4
  
- x = (-b +/- sqrt(b^2 - 4*a*c))/(2*a)
+ Given the coefficients a, b and c, calculate the solutions to a quadratic equation with these coefficients. Take into account the different number of solutions (0, 1 or 2). If you need a math refresher, this Wikipedia article on the quadratic equation will help
+ 
+ https://en.wikipedia.org/wiki/Quadratic_formula.
  */
 
 let a = 1.0
@@ -142,6 +159,52 @@ if d > 0 {
     print("No solution")
 }
 
+/*:
+ ### Challenge 5
+ 
+ Given a month (represented with a String in all lowercase) and the current year (represented with an Int), calculate the number of days in the month. Remember that because of leap years, "february" has 29 days when the year is a multiple of 4 but not a multiple of 100. February also has 29 days when the year is a multiple of 400.
+ */
 
+let month = "february"
+let year = 2018
+var numberOfDays = 0
+if month == "january" || month == "march" || month == "may" || month == "july" || month == "august" || month == "october" || month == "december" {
+    numberOfDays = 31
+} else if month == "february" {
+    numberOfDays = ((year % 4 == 0 && year % 100 != 0) || (year % 400 == 0)) ? 29 : 28
+} else {
+    numberOfDays = 30
+}
+print("\(numberOfDays)")
+
+/*:
+ ### Challenge 6
+ Given a number, determine if this number is a power of 2. (Hint: you can use log2(number) to find the base 2 logarithm of number. log2(number) will return a whole number if number is a power of two. You can also solve the problem using a loop and no logarithm.)
+ 
+ */
+let value = 1025
+var num = value
+var isPowerOfTwo = true
+while (num % 2 == 0) {
+     num /= 2
+}
+if num == 1 {
+    print("\(value) is a power of 2")
+} else {
+    print("\(value) is a not power of 2")
+}
+
+// Using double values..
+let doubleValue = 1024.0
+var doubleNum = doubleValue
+
+while doubleNum.truncatingRemainder(dividingBy: 2) == 0 {
+    doubleNum /= 2
+}
+if (doubleNum == 1) {
+    print("\(doubleValue) is a power of 2")
+} else {
+    print("\(doubleValue) is a not power of 2")
+}
 
 
