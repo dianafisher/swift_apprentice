a4b44e1393e961b515cc2999de57bdc8b26818a4
Diana Fisher <diana.fisher@gmail.com>
2018-04-03 15:20:03 -0700

    Mini-exercises 1 and 2.

--- Chapter8.playground/Contents.swift
+++ Chapter8.playground/Contents.swift
@@ -163,4 +163,21 @@ let combined = names2.reduce("") {
 }
 print(combined)
 
+/*
+ ##2. Using the same names array, first filter the array to contain only names which have more than four characters in them, and then create the same concatenation of names as in the above exercise. (Hint: you can chain these operations together.)
+ */
 
+let filtered = names2.filter {
+    return $0.count > 4
+    }.reduce("") {
+        $0 + $1
+}
+print(filtered)
+
+/*:
+ ## 3. Create a constant dictionary called namesAndAges which contains some names as strings mapped to ages as integers. Now use filter to create a dictionary containing only people under the age of 18.
+ */
+
+/*:
+ ## 4. Using the same namesAndAges dictionary, filter out the adults (those 18 or older) and then use map to convert to an array containing just the names (i.e. drop the ages).
+ */
